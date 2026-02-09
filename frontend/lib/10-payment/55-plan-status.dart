e0df3bec80b4be21d4fd98bb7bb2bb83ff0cec79
oyu1731 <ktq2390303@stu.o-hara.ac.jp>
2026-02-09 09:49:33 +0900

    planstatus修正

--- frontend/lib/10-payment/55-plan-status.dart
+++ frontend/lib/10-payment/55-plan-status.dart
@@ -304,13 +304,13 @@ class _PlanStatusScreenState extends State<PlanStatusScreen>
                               ],
                             ),
                             const SizedBox(height: 4),
-                            Text(
-                              'ID: $_userId',
-                              style: TextStyle(
-                                fontSize: 14,
-                                color: Colors.grey.shade600,
-                              ),
-                            ),
+                            // Text(
+                            //   'ID: $_userId',
+                            //   style: TextStyle(
+                            //     fontSize: 14,
+                            //     color: Colors.grey.shade600,
+                            //   ),
+                            // ),
                           ],
                         ),
                       ),
